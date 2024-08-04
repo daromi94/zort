d1e14dc77c9d9392bf84979fd91f6cb1a7db82c0
daromi94 <davromero07@gmail.com>
2024-08-03 20:01:12 -0500

    feat: :sparkles: read lines

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <sysexits.h>
@@ -13,6 +14,8 @@ void write_usage();
 std::vector<std::string_view> parse_file_paths(int argc, char *argv[]);
 
 bool check_file_paths_exist(const std::vector<std::string_view> &file_paths);
+
+std::tuple<bool, std::vector<std::string>> read_lines(const std::vector<std::string_view> &file_paths);
 
 int main(int argc, char *argv[]) {
   if (argc < MINIMUM_ARGUMENT_COUNT) {
@@ -30,6 +33,20 @@ int main(int argc, char *argv[]) {
     return EX_NOINPUT;
   }
 
+  const auto [lines_ok, lines] = read_lines(file_paths);
+  if (!lines_ok) {
+    return EX_IOERR;
+  }
+
+  if (lines.empty()) {
+    std::clog << PROGRAM << ": No lines to process" << std::endl;
+    return EX_OK;
+  }
+
+  for (const auto &line : lines) {
+    std::cout << line << std::endl;
+  }
+
   return EX_OK;
 }
 
@@ -42,7 +59,6 @@ std::vector<std::string_view> parse_file_paths(int argc, char *argv[]) {
 
   for (int i = 1; i < argc; ++i) {
     std::string_view argument(argv[i]);
-    // TODO: validate file path arguments
     if (argument.starts_with('-')) {
       break;
     }
@@ -55,10 +71,31 @@ std::vector<std::string_view> parse_file_paths(int argc, char *argv[]) {
 bool check_file_paths_exist(const std::vector<std::string_view> &file_paths) {
   for (const auto &file_path : file_paths) {
     if (!std::filesystem::exists(file_path)) {
-      std::cerr << PROGRAM << ": No such file or directory (" << file_path << ")" << std::endl;
+      std::cerr << PROGRAM << ": No such file (" << file_path << ")" << std::endl;
       return false;
     }
   }
 
   return true;
+}
+
+std::tuple<bool, std::vector<std::string>> read_lines(const std::vector<std::string_view> &file_paths) {
+  std::vector<std::string> lines;
+
+  for (const auto &file_path : file_paths) {
+    std::ifstream file(file_path);
+    if (!file) {
+      std::cerr << PROGRAM << ": Unable to open file (" << file_path << ")" << std::endl;
+      return {false, lines};
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+      lines.emplace_back(line);
+    }
+
+    file.close();
+  }
+
+  return {true, lines};
 }
